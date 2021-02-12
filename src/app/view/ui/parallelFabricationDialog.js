1b2dcbca2ab309e28eaa5d18dae2b7ce5fe6ec81
bjoernh2000 <bjoernh@bu.edu>
2021-02-12 19:59:56 +0100

    added some axios

--- src/app/view/ui/parallelFabricationDialog.js
+++ src/app/view/ui/parallelFabricationDialog.js
@@ -1,6 +1,8 @@
 import dialogPolyfill from "dialog-polyfill";
 import * as Registry from "../../core/registry";
 import axios from 'axios';
+import JSZip from "jszip";
+import ManufacturingLayer from "../../manufacturing/manufacturingLayer";
 
 export default class ParallelFabricateDialog {
     constructor() {
@@ -20,17 +22,16 @@ export default class ParallelFabricateDialog {
         });
 
         this.__sendFabricationSubmissionButton.onclick = function() {
+            console.log("send designs");
             let email = document.getElementById("fabricate_dialog_email_field").value;
             let address = document.getElementById("fabricate_dialog_address_field").value;
 
-            // let endpoint = "";
+            let endpoint = "http://127.0.0.1:5000/endpoint";
 
             let svgs = Registry.viewManager.layersToSVGStrings();
             for (let i = 0; i < svgs.length; i++) {
                 svgs[i] =
-                    ManufacturingLayer.generateSVGTextPrepend(Registry.currentDevice.getXSpan(), Registry.currentDevice.getYSpan()) +
-                    svgs[i] +
-                    ManufacturingLayer.generateSVGTextAppend();
+                    ManufacturingLayer.generateSVGTextPrepend(Registry.currentDevice.getXSpan(), Registry.currentDevice.getYSpan()) + svgs[i] + ManufacturingLayer.generateSVGTextAppend();
             }
 
             let blobs = [];
@@ -44,6 +45,29 @@ export default class ParallelFabricateDialog {
             }
 
             if (success == 0) throw new Error("Unable to generate any valid SVGs. Do all layers have at least one non-channel item in them?");
+            else {
+                let content = zipper.generate({
+                    type: "blob"
+                });
+
+                axios.post(endpoint, {
+                    "email": email,
+                    "acceptance": "n/a",
+                    "completion": "not completed",
+                    "time": 10,
+                    "cost": 1000,
+                    "file": content,
+                    "address": address
+                    })
+                    .then((res) => {
+                        console.log(res);
+                        alert("Add the submission code here")
+                    })
+                    .catch((err) => {
+                        console.error(err);
+                        alert("Error submiting the design for fabrication:" + err.message)
+                    })
+            }
 
             
         }
